b90595b489a5ce267721c4df69311784c298fc03
Mohin-Uddin-Mishrat <mmishrat90@gmail.com>
2025-10-21 15:53:51 +0600

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,43 +1,44 @@
-import express, { Request, Response } from 'express'
-import cors from 'cors';
-import globalErrorHandler from './app/middlewares/global_error_handler'
-import notFound from './app/middlewares/not_found_api'
-import cookieParser from 'cookie-parser'
-import appRouter from './routes'
-import { setupSwagger } from './app/configs/swagger';
+import express, { Request, Response } from "express";
+import cors from "cors";
+import globalErrorHandler from "./app/middlewares/global_error_handler";
+import notFound from "./app/middlewares/not_found_api";
+import cookieParser from "cookie-parser";
+import appRouter from "./routes";
+import { setupSwagger } from "./app/configs/swagger";
 
 // define app
-const app = express()
+const app = express();
 
 // middleware
-app.use(cors({
-    origin: ["http://localhost:5173",
-        "http://localhost:5174/",
-        "http://localhost:5175/",
-        "http://localhost:5176/",
-        "http://localhost:3000/",
-        "https://eric-rivera-front-end.netlify.app/"
-
-    ]
-}))
-setupSwagger(app)
-app.use(express.json())
-app.use(cookieParser())
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174/",
+      "http://localhost:5175/",
+      "http://localhost:5176/",
+      "http://localhost:3000/",
+      "https://eric-rivera-front-end.netlify.app/",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+setupSwagger(app);
+app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-
-app.use("/api/v1", appRouter)
+app.use("/api/v1", appRouter);
 
 // stating point
-app.get('/', (req: Request, res: Response) => {
-    res.status(200).json({
-        status: 'success',
-        message: 'Server is running successful !!',
-        data: null,
-    });
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    status: "success",
+    message: "Server is running successful !!",
+    data: null,
+  });
 });
-
-
 
 // global error handler
 app.use(globalErrorHandler);
